5c446e6f922ed9cfea7da44869303dc8c19289f9
FreememGetmem <morndour83@hotmail.com>
2023-11-13 10:08:52 -0500

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from EtoEmlProject import logger
+from EtoEmlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logging")
+
+STAGE_NAME = 'Data Ingestion Stage'
+
+try:
+    logger.info(f'>>>>>>> Stage {STAGE_NAME} started <<<<<<<<')
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f'>>>>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nX===============')
+except Exception as e:
+    logger.exception(e)
+    raise e 
